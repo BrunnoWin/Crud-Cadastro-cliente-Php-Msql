f65e20fb645e6f5dce7f82032285c4abfb01d135
Brunno Henrique Vilas Boas <99026853+BrunnoWin@users.noreply.github.com>
2022-07-04 21:15:06 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 include_once('include/Database.php');
-define('SS_DB_NAME', 'dadoscliente');//banco da dados
+define('SS_DB_NAME', 'cliente');//banco da dados
 define('SS_DB_USER', 'root');//usuario banco
 define('SS_DB_PASSWORD', '');///senha
 define('SS_DB_HOST', 'localhost');//servidor
